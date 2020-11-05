356f7eb1043c11d48e46d4e6d1da96b2a687ceaa
alyssa-t <alyssa@poli.ufrj.br>
2020-11-05 14:08:56 -0300

    remake extractCaption.py

---
+++ GenerateCaptions/__main__.py
@@ -0,0 +1,39 @@
+import time
+import os
+import sys
+import json
+import warnings
+
+CAPTION_JSON_FILE = "/.../captions_val2017.json"
+OUTPUT_FILE_PATH = "/.../"
+CAPTION_TXT_FILENAME = "caption"
+NUM_CAPTION_PER_FILE = 18000
+
+def main():
+	counter = 1
+	i = 1
+
+	with open(CAPTION_JSON_FILE) as json_file:
+		captionFile = json.load(json_file)
+		f = open(OUTPUT_FILE_PATH + CAPTION_TXT_FILE + "_" + str(i) + ".txt", "w")
+
+		for p in captionFile['annotations']:
+			f.write(p['caption'])
+			counter+=1
+			
+			if (counter > len(captionFile['annotations'])):
+				f.close()
+				break
+
+			if (counter % NUM_CAPTION_PER_FILE  == 0):
+				f.close()
+				i+=1
+				f = open(OUTPUT_FILE_PATH + CAPTION_TXT_FILE + "_" + str(i) + ".txt", "w")
+			elif ("\n" not in p['caption']):
+				f.write("\n")
+
+if __name__ == '__main__':
+	warnings.simplefilter(action='ignore', category=FutureWarning)
+	main()
+
+
